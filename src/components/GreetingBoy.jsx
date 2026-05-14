4311cec7387c8b2a60f96ae3ef9467e9c0c9cbe2
Robert Toroitich <robertojnr2802@gmail.com>
2026-05-14 17:17:16 +0300

    Added features for avatar greeting component

--- src/components/GreetingBoy.jsx
+++ src/components/GreetingBoy.jsx
@@ -1,16 +1,29 @@
-import React from 'react';
+import React, { useState, useEffect, use } from 'react';
 import '../styles/greetingboy.css';
 
-const GreetingBoy = ({ greeting = "Hello there!" }) => {
+const GreetingBoy = ({ greeting = "Hello!" }) => {
+  const [mood, setMood] = useState('happy');
+  const [trigger, setTrigger] = useState(false)
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setMood('bored');
+    }, 22000);
+
+    return () => clearTimeout(timer);
+  }, [mood]);
+
   return (
-    
-    <div className="greeting-container">
-      {/* Dynamic greeting bubble */}
-      <div className="speech-bubble">
+    <div 
+      className="greeting-container" 
+      onMouseEnter={() => setMood('happy')}
+    >
+      {/* Bubble only shows if happy */}
+      <div className={`speech-bubble ${mood === 'happy' ? 'show' : ''}`}>
         {greeting}
       </div>
 
-      <div className="boy-simple">
+      <div className={`boy-simple ${mood}`}>
         <div className="boy__head-simple">
           <div className="boy__hair-simple"></div>
           <div className="boy__eyes-simple"></div>
@@ -20,7 +33,6 @@ const GreetingBoy = ({ greeting = "Hello there!" }) => {
         <div className="boy__arm-wave"></div>
       </div>
     </div>
-    
   );
 };
 
